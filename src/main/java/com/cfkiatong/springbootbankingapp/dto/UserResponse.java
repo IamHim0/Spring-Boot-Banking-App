b840ec8d6114f9933c268db8eaadbf7bf578447b
Cyrill Dave Kiat-ong <cfkiatong@up.edu.ph>
2026-02-20 10:58:12 +0800

    refactor(docker): container,volume,db names updated

--- src/main/java/com/cfkiatong/springbootbankingapp/dto/UserResponse.java
+++ src/main/java/com/cfkiatong/springbootbankingapp/dto/UserResponse.java
@@ -6,28 +6,28 @@ import java.util.UUID;
 
 public class UserResponse {
 
-    private String email;
     private String username;
+    private String email;
     private String firstName;
     private String lastName;
     private Set<Role> roles;
     private List<UUID> accountIds;
 
-    public UserResponse(String email, String username, String firstName, String lastName, Set<Role> roles, List<UUID> accountIds) {
-        this.email = email;
+    public UserResponse(String username, String email, String firstName, String lastName, Set<Role> roles, List<UUID> accountIds) {
         this.username = username;
+        this.email = email;
         this.firstName = firstName;
         this.lastName = lastName;
         this.roles = roles;
         this.accountIds = accountIds;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
     public String getUsername() {
         return username;
+    }
+
+    public String getEmail() {
+        return email;
     }
 
     public String getFirstName() {
