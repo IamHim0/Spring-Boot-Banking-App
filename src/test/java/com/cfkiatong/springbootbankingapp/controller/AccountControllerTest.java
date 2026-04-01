1a19a9df4af476b9a12872b5ea54caf25d99af44
Cyrill Dave Kiat-ong <cfkiatong@up.edu.ph>
2026-04-01 22:33:40 +0800

    feature(unit tests): added happy paths to TransactionControllerTest

--- src/test/java/com/cfkiatong/springbootbankingapp/controller/AccountControllerTest.java
+++ src/test/java/com/cfkiatong/springbootbankingapp/controller/AccountControllerTest.java
@@ -1,4 +1,79 @@
 package com.cfkiatong.springbootbankingapp.controller;
 
+import com.cfkiatong.springbootbankingapp.dto.response.AccountResponse;
+import com.cfkiatong.springbootbankingapp.services.AccountService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
+import org.springframework.boot.security.autoconfigure.web.servlet.SecurityFilterAutoConfiguration;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+import java.math.BigDecimal;
+import java.net.URI;
+import java.util.UUID;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.http.RequestEntity.post;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.client.ExpectedCount.times;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(
+        controllers = AccountController.class,
+        excludeAutoConfiguration = {
+                SecurityAutoConfiguration.class,
+                SecurityFilterAutoConfiguration.class
+        }
+)
+@AutoConfigureMockMvc(addFilters = false)
+@ExtendWith(MockitoExtension.class)
 public class AccountControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockitoBean
+    private AccountService accountService;
+
+    private final UUID userId = UUID.fromString("00000000-0000-0000-0000-000000000001");
+    private final String owner = "owner";
+    private final UUID accountId = UUID.fromString("00000000-0000-0000-0000-000000000002");
+    private final BigDecimal balance = BigDecimal.valueOf(10000);
+    private AccountResponse accountResponse;
+
+    @BeforeEach
+    void setUp() {
+        accountResponse = new AccountResponse(
+                accountId,
+                owner,
+                balance
+        );
+    }
+
+//    @Test
+//    @WithMockUser(username = "00000000-0000-0000-0000-000000000001")
+//    @DisplayName("POST /accounts — authenticated user — returns 201 with Location header")
+//    void createAccount_validUser_returnsAccountResponseAndEndpoint() throws Exception {
+//        when(accountService.createAccount(userId)).thenReturn(accountResponse);
+//
+//        mockMvc.perform(post(String.valueOf(URI.create("/api/v1/users/me/accounts"))))
+//                .with(user(userId.toString()));
+//
+//        verify(accountService).createAccount(userId);
+//    }
 }
--- src/test/java/com/cfkiatong/springbootbankingapp/controller/TransactionControllerTest.java
+++ src/test/java/com/cfkiatong/springbootbankingapp/controller/TransactionControllerTest.java
@@ -1,51 +1,186 @@
 package com.cfkiatong.springbootbankingapp.controller;
 
+import com.cfkiatong.springbootbankingapp.config.SecurityConfiguration;
+import com.cfkiatong.springbootbankingapp.dto.TransactionType;
+import com.cfkiatong.springbootbankingapp.dto.request.TransactionRequest;
 import com.cfkiatong.springbootbankingapp.dto.response.BalanceResponse;
+import com.cfkiatong.springbootbankingapp.dto.response.TransactionResponse;
+import com.cfkiatong.springbootbankingapp.entity.Transaction;
+import com.cfkiatong.springbootbankingapp.security.CustomAuthenticationEntryPoint;
+import com.cfkiatong.springbootbankingapp.security.jwt.JwtFilter;
+import com.cfkiatong.springbootbankingapp.security.jwt.JwtService;
 import com.cfkiatong.springbootbankingapp.services.TransactionService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.MediaType;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
+import org.springframework.boot.security.autoconfigure.web.servlet.SecurityFilterAutoConfiguration;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(controllers = {TransactionController.class})
-public class TransactionControllerTest {
+@WebMvcTest(
+        controllers = TransactionController.class,
+        excludeAutoConfiguration = {
+                SecurityAutoConfiguration.class,
+                SecurityFilterAutoConfiguration.class
+        })
+@AutoConfigureMockMvc(addFilters = false)
+@Import({
+        SecurityConfiguration.class,
+        JwtFilter.class
+})
+class TransactionControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
-    @Autowired
-    private ObjectMapper objectMapper;
+
     @MockitoBean
     private TransactionService transactionService;
 
-    private final UUID accountId = UUID.randomUUID();
+    @MockitoBean
+    private UserDetailsService userDetailsService;
+
+    @MockitoBean
+    private JwtService jwtService; // Required because JwtFilter is imported
+
+    @MockitoBean
+    private CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+
+    private final String stringUserId = "11111111-1111-1111-1111-111111111111";
+    private final UUID userId = UUID.fromString(stringUserId);
+    private final UUID accountId = UUID.fromString("22222222-2222-2222-2222-222222222222");
+    private final UUID targetAccountId = UUID.fromString("33333333-3333-3333-3333-333333333333");
 
     @Test
-    @WithMockUser(username = "48f90b11-9656-4cfb-ace4-107a2da90970")
-        // ✅ Cleaner than .with(user(...))
-    void getBalance_validRequest_returnsBalance() throws Exception {
-        BalanceResponse response = new BalanceResponse(new BigDecimal("1000.00"));
+    @WithMockUser(username = "11111111-1111-1111-1111-111111111111")
+    void getBalance_ValidUser_ReturnsBalance() throws Exception {
+        when(transactionService.getBalance(userId, accountId))
+                .thenReturn(new BalanceResponse(new BigDecimal("1500.00")));
 
-        // ✅ Use any() because the controller reconstructs the UUID internally
-        when(transactionService.getBalance(any(UUID.class), any(UUID.class)))
-                .thenReturn(response);
-
-        mockMvc.perform(get("/api/v1/users/me/accounts/{accountId}/transactions", accountId))
+        mockMvc.perform(get("/api/v1/users/me/accounts/{accountId}/transactions/balance", accountId))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.balance").value(1000.00)); // ✅ Correct jsonPath import
-
-        verify(transactionService).getBalance(any(UUID.class), any(UUID.class));
+                .andExpect(jsonPath("$.balance").value("1500.0"));
     }
+
+    @Test
+    @WithMockUser(username = "11111111-1111-1111-1111-111111111111")
+    void makeDepositTransaction_ValidTransaction_ReturnBalance() throws Exception {
+        TransactionRequest transactionRequest = new TransactionRequest(
+                TransactionType.DEPOSIT,
+                new BigDecimal("100.00"),
+                null
+        );
+
+        when(transactionService.makeTransaction(userId, accountId, transactionRequest))
+                .thenReturn(new BalanceResponse(new BigDecimal("1600.00")));
+
+        mockMvc.perform(post("/api/v1/users/me/accounts/{accountId}/transactions", accountId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(transactionRequest)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.balance").value("1600.0"));
+    }
+
+    @Test
+    @WithMockUser(username = "11111111-1111-1111-1111-111111111111")
+    void makeWithdrawalTransaction_ValidTransaction_ReturnBalance() throws Exception {
+        TransactionRequest transactionRequest = new TransactionRequest(
+                TransactionType.WITHDRAWAL,
+                new BigDecimal("100.00"),
+                null
+        );
+
+        when(transactionService.makeTransaction(userId, accountId, transactionRequest))
+                .thenReturn(new BalanceResponse(new BigDecimal("1400.00")));
+
+        mockMvc.perform(post("/api/v1/users/me/accounts/{accountId}/transactions", accountId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(transactionRequest)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.balance").value("1400.0"));
+    }
+
+    @Test
+    @WithMockUser(username = "11111111-1111-1111-1111-111111111111")
+    void makeTransferTransaction_ValidTransaction_ReturnBalance() throws Exception {
+        TransactionRequest transactionRequest = new TransactionRequest(
+                TransactionType.TRANSFER,
+                new BigDecimal("100.00"),
+                targetAccountId
+        );
+
+        when(transactionService.makeTransaction(userId, accountId, transactionRequest))
+                .thenReturn(new BalanceResponse(new BigDecimal("1400.00")));
+
+        mockMvc.perform(post("/api/v1/users/me/accounts/{accountId}/transactions", accountId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(transactionRequest)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.balance").value("1400.0"));
+    }
+
+    @Test
+    @WithMockUser(username = "11111111-1111-1111-1111-111111111111")
+    void getUserTransactions_ValidUser_ReturnsTransactionList() throws Exception {
+        TransactionResponse transactionResponse = new TransactionResponse(
+                LocalDateTime.now(),
+                UUID.randomUUID(),
+                TransactionType.DEPOSIT,
+                accountId,
+                null,
+                new BigDecimal("100.00"),
+                new BigDecimal("0.00"),
+                new BigDecimal("100.00")
+        );
+
+        List<TransactionResponse> mockTransactions = List.of(transactionResponse);
+
+        when(transactionService.getUserTransactions(userId))
+                .thenReturn(mockTransactions);
+
+        mockMvc.perform(get("/api/v1/users/me/accounts/{accountId}/transactions/userhistory", accountId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray());
+    }
+
+    @Test
+    @WithMockUser(username = "11111111-1111-1111-1111-111111111111")
+    void getAccountTransactions_ValidUser_ReturnsTransactionList() throws Exception {
+        TransactionResponse transactionResponse = new TransactionResponse(
+                LocalDateTime.now(),
+                UUID.randomUUID(),
+                TransactionType.DEPOSIT,
+                accountId,
+                null,
+                new BigDecimal("100.00"),
+                new BigDecimal("0.00"),
+                new BigDecimal("100.00")
+        );
+
+        List<TransactionResponse> mockTransactions = List.of(transactionResponse);
+
+        when(transactionService.getAccountTransactions(userId, accountId)).thenReturn(mockTransactions);
+
+        mockMvc.perform(get("/api/v1/users/me/accounts/{accountId}/transactions/accounthistory", accountId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray());
+    }
+
 }
